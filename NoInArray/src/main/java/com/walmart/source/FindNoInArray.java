6698c4f1512b24982a7a5d5f017de59456853aa8
Dhriti Sengupta <Dhriti.Sengupta@walmartlabs.com>
2018-10-24 17:15:29 +0530

    Added tests

--- NoInArray/src/main/java/com/walmart/source/FindNoInArray.java
+++ NoInArray/src/main/java/com/walmart/source/FindNoInArray.java
@@ -2,14 +2,13 @@ package com.walmart.source;
 
 public class FindNoInArray {
 
-    public static void main(String[] args) {
+    public static void findNo(int noToFind) {
         int[] array = {1, 2, 3, 4, 5};
-        int noToFind = 6;
         boolean found = false;
         for (int i=0; i < array.length; i++) {
             if (array[i] == noToFind )
                 found = true;
         }
-        System.out.println("found : " + found);
+        System.out.println("found " + noToFind + " : " + found);
     }
 }
---
+++ NoInArray/src/test/java/com/walmart/source/FindNoInArrayTest.java
@@ -0,0 +1,15 @@
+package com.walmart.source;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindNoInArrayTest {
+
+    @Test
+    public void main() {
+        FindNoInArray.findNo(2);
+        FindNoInArray.findNo(9);
+        FindNoInArray.findNo(12);
+    }
+}
